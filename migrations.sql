eccf23d95e67dc6342e943e77c6fb66836bd312d
Javier Casas <javcasas@gmail.com>
2018-03-27 09:18:28 -0500

    Some comments

--- migrations.sql
+++ migrations.sql
@@ -1,4 +1,5 @@
 -- API definition
+-- This is exposed by PostgREST
 create schema api;
 create table api.todos (
   id serial primary key,
@@ -24,8 +25,9 @@ grant usage on schema api to todo_user;
 grant all on api.todos to todo_user;
 grant usage, select on sequence api.todos_id_seq to todo_user;
 
--- Authentication
+-- Authentication schema
 create schema auth;
+-- Both web_anon and todo_user can user the authentication schema
 grant usage on schema auth to web_anon, todo_user;
 
 create or replace function auth.check_token() returns void
