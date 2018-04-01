449c38135a13b5bad5e4256443b17d61557d1f79
Javier Casas <javcasas@gmail.com>
2018-03-31 22:50:02 -0500

    More consistent tests

--- database/tests/migrations/test_migrations.py
+++ database/tests/migrations/test_migrations.py
@@ -1,4 +1,6 @@
 import test_base
+import requests
+import jwt
 
 class TestMigrationsRunProperly(test_base.TestBase):
 
@@ -18,3 +20,36 @@ class TestMigrationsRunProperly(test_base.TestBase):
         self.assertIn(("app_user",), roles)
         self.assertIn(("web_anon",), roles)
         self.assertIn(("todo_user",), roles)
+
+    def assertSuccessWith(self, req, to_compare):
+        self.assertEqual(req.status_code, 200)
+        self.assertEqual(req.json(), to_compare)
+
+    def assertNoAuth(self, req, to_compare):
+        self.assertEqual(req.status_code, 401)
+        res = req.json()
+        self.assertIsNone(res.get('hint'))
+        self.assertIsNone(res.get('details'))
+        self.assertTrue(res['message'].startswith('permission denied for relation'))
+        self.assertEqual(res['code'], '42501')
+
+    def test_get_todos(self):
+        req = requests.get("http://localhost:3000/todos")
+        self.assertSuccessWith(req, [
+            {
+                'done': False,
+                'due': None,
+                'id': 1,
+                'task': 'finish tutorial 0'
+            },
+            {
+                'done': False,
+                'due': None,
+                'id': 2,
+                'task': 'pat self on back'
+            }
+        ])
+
+    def test_fail_patch_todos(self):
+        req = requests.patch("http://localhost:3000/todos", {"done": True})
+        self.assertNoAuth(req, "a")
--- database/tests/wait_for_postgres.py
+++ database/tests/wait_for_postgres.py
@@ -4,6 +4,7 @@ import time
 import requests
 import psycopg2
 from psycopg2.extensions import parse_dsn
+import requests
 
 def waitForPostgres(timeout=20):
     for i in range(timeout * 10):
@@ -15,5 +16,18 @@ def waitForPostgres(timeout=20):
         except psycopg2.OperationalError:
             time.sleep(0.1)
 
+def waitForPostgREST(timeout=20):
+    for i in range(timeout * 10):
+        req = requests.get("http://localhost:3000/", timeout=0.1)
+        if req.status_code == 400:
+            # If there is no 'web_anon' role, it returns an error 400
+            # but by then PostgREST is able to query PostgreSQL
+            return
+        time.sleep(0.1)
+    else:
+        raise Exception("PostgREST not available")
+
 waitForPostgres()
 waitForPostgres()
+waitForPostgres()
+waitForPostgREST()
